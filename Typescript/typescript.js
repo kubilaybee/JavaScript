6f2d680fc28e48c4bf4e1bb50d08cde8cdf50ecb
kubilaybee <kubilaybee@gmail.com>
2022-03-04 06:16:53 +0300

    arrays

--- Typescript/typescript.js
+++ Typescript/typescript.js
@@ -7,4 +7,16 @@
 // c = true;
 // d = 32;
 // console.log(a, b, c, d);
-let;
+// arrays
+// let langs: string[];
+// let numbers: number[];
+// let boolValues: Array<boolean>;
+// let a: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8];
+// langs = ["php", "java", "c++", "python"];
+// numbers = [1, 2, 3, 4, 5];
+// boolValues = [true, false, false];
+// console.log(...langs);
+// console.log(...numbers);
+// console.log(...boolValues);
+// console.log(...a);
+// functions
--- Typescript/typescript.ts
+++ Typescript/typescript.ts
@@ -9,5 +9,18 @@
 // d = 32;
 
 // console.log(a, b, c, d);
+// arrays
+// let langs: string[];
+// let numbers: number[];
+// let boolValues: Array<boolean>;
+// let a: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8];
 
-let;
+// langs = ["php", "java", "c++", "python"];
+// numbers = [1, 2, 3, 4, 5];
+// boolValues = [true, false, false];
+
+// console.log(...langs);
+// console.log(...numbers);
+// console.log(...boolValues);
+// console.log(...a);
+// functions
