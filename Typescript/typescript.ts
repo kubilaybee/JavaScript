7fcdd6da55a77020e35c9e5196ebe04b225cc00c
kubilaybee <kubilaybee@gmail.com>
2022-03-04 06:12:11 +0300

    typescript start

---
+++ Typescript/typescript.ts
@@ -0,0 +1,13 @@
+// let a: string;
+// let b: number;
+// let c: boolean;
+// let d: any; //dinamik
+
+// a = "Hello World";
+// b = 22;
+// c = true;
+// d = 32;
+
+// console.log(a, b, c, d);
+
+let;
